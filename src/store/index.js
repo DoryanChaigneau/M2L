c86ba5d23c53d4b3c7f884637dcf65bdef91d6f7
Doryan <doryan.chaigneau@hotmail.fr>
2021-03-04 11:10:58 +0100

    Initial Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: ""
   },
   mutations: {
   },
   actions: {
+    login({ state}, form) {
+      axios.post('auth', {
+        "login": form.login,
+        "motDePasse": form.password
+      }).then(response => {
+        console.log((response))
+        state.token = response.data.token;
+      }).catch(
+          error => {
+            if (error) throw error
+          }
+      )
+    }
   },
   modules: {
   }
