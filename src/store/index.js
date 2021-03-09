1d40ff8054f00399fc0b8de2cb4ec4b0d26bb552
Doryan <doryan.chaigneau@hotmail.fr>
2021-03-09 15:54:25 +0100

    connection

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import route from "../router";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: ""
+    token: "",
+    status: ""
   },
   mutations: {
   },
   actions: {
-    login({ state}, form) {
+    auth({ state }, form) {
+      state.status = ""
       axios.post('auth', {
         "login": form.login,
         "motDePasse": form.motDePasse
       }).then(response => {
         state.token = response.data.token;
+        state.status = "200";
+        route.push('/home');
       }).catch(
           error => {
+            state.status = "401";
             if (error) throw error
           }
       )
