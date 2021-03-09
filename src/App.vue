2b901203af70ef467e0280d88065525d581fafd0
fabienD <fabien.drapeau1@epsi.fr>
2021-03-09 15:57:53 +0100

    fix connection page

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,6 @@
         width="40"
       />
       <v-spacer></v-spacer>
-      <v-btn
-        v-if="show_Button"
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main background color= "red">
       <router-view />
