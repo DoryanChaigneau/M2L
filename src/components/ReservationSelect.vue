f9bb5f31525cf7f6b562fc359288b75aaad9a151
maxime <maxime.lemaire@epsi.fr>
2021-03-11 09:38:52 +0100

    modif design resa

--- src/components/ReservationSelect.vue
+++ src/components/ReservationSelect.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <h1 style="text-align: center">Liste des salles disponible : </h1>
+    <div class="col-md-8 offset-md-2" style="margin-top:50px;">
+
+        <div class="card-header text-center">
+          <h1>Liste des salles disponible :</h1>
+        </div>
     <div v-for="item in $store.state.salles" :key="item.id" class="card">
-      <div class="card-body">
+      <div class="card-body" style="text-align: center">
         <h3 class="card-title">{{ item.libelle }}</h3>
         <h5 class="card-title">{{ item.adresse }}</h5>
         <p class="card-text"><b>Description :</b> {{ item.description }}</p>
@@ -12,6 +16,9 @@
       </div>
     </div>
   </div>
+    </div>
+
+
 </template>
 <script>
 
